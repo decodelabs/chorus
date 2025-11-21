6eb6ff65399e77402e54c5ec635e675c208f940d
Tom Wright <tom@inflatablecookie.com>
2025-11-21 10:34:38 +0000

    Updated repo shape
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Sample.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * This file exists as a temporary workaround to give PHPStan and other Effigy controlled tools
+ * something to chew on at release time. It can be safely removed once Effigy supports
+ * management of release process actions
+ */
